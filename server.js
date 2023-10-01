55cd6c502fc39999b6ad59410cbe57f0aa8b0d7c
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2023-10-01 09:43:09 +0700

    ubah code di server.js dan ubah nama folder views

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const { mongoose } = require("./models/database.js");
 const { User } = require("./models/user.js");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10000mb", parameterLimit: 100000 }));
+
+// ubah jika menggunakan react
+app.use(express.static(path.join(__dirname, "views")));
 
 app.listen(3000, () => {
   console.log("Server started on http://localhost:3000");
+
+  app.get("/", (req, res) => {
+    res.sendFile("index.html");
+  });
 });
